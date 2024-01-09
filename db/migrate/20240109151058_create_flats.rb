018d08b078db2f3fc739be3f4e73abbcc1e58a4f
luis sensat <luis.sensat@gmail.com>
2024-01-09 16:25:01 +0100

    created users, flats and bookings models

---
+++ db/migrate/20240109151058_create_flats.rb
@@ -0,0 +1,15 @@
+class CreateFlats < ActiveRecord::Migration[7.1]
+  def change
+    create_table :flats do |t|
+      t.string :title
+      t.text :description
+      t.integer :size
+      t.integer :price
+      t.string :location
+      t.float :rating
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
