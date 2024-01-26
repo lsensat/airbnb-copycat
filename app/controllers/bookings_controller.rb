2c9a1b3a9f7d5e9cceb154f330d304c6aa731018
luis sensat <luis.sensat@gmail.com>
2024-01-26 14:31:53 +0100

    fixed lines

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -31,6 +31,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_time, :end_time, booking_dates_attributes: [:id, :date, :price])
+    params.require(:booking).permit(:start_time, :end_time)
   end
 end
