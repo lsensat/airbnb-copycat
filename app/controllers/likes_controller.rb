0e0b0bc2003dc62bf1418ca6b8f65ed63f11224a
luis sensat <luis.sensat@gmail.com>
2024-01-28 13:47:11 +0100

    does like, but many errors and cannot destroy it yet

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,41 @@
 class LikesController < ApplicationController
-  def create
-    flat = Flat.find(params[:flat_id])
-    # @like = Like.new(like_params)
+  before_action :flat_find, only: %i[create destroy]
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
 
-    like = Like.new(user: current_user, flat_id: flat)
+  def create
+    @like = current_user.likes.new(flat: @flat)
 
     respond_to do |format|
-      if like.save
-        format.html { redirect_to flats_path }
+      if @like.save
+        format.html { redirect_to flats_path(@like) }
         format.json # Follows the classic Rails flow and look for a create.json view
       else
-        format.html { render flats_path, status: :unprocessable_entity }
+        format.html { render "flats", status: :unprocessable_entity }
         format.json # Follows the classic Rails flow and look for a create.json view
       end
     end
   end
 
+  def destroy
+    @like = current_user.likes.destroy(flat: @flat)
 
+    respond_to do |format|
+      if @like.save
+        format.html { redirect_to flats_path(@like) }
+        format.json # Follows the classic Rails flow and look for a create.json view
+      else
+        format.html { render "flats", status: :unprocessable_entity }
+        format.json # Follows the classic Rails flow and look for a create.json view
+      end
+    end
+  end
 
   private
+
+  def flat_find
+    @flat = Flat.find(params[:flat_id])
+  end
 
   def like_params
     params.require(:like).permit(:user_id, :flat_id)
--- app/javascript/controllers/like_controller.js
+++ app/javascript/controllers/like_controller.js
@@ -15,15 +15,68 @@ export default class extends Controller {
     this.likeTarget.classList.toggle("fa-solid")
     this.likeTarget.classList.toggle("fa-regular")
 
-    const url = `/flats/${this.flatValue}/likes/create`
+    const url = `/flats/${this.flatValue}/likes`
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    console.log(token)
 
     const content = {
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({ 'flat_id': this.flatValue })
+      body: JSON.stringify({
+        "flat_id": this.flatValue,
+        'X-CSRF-Token': token,
+      })
     }
 
     fetch(url, content)
-    .then(response => console.log(response.json()))
+    .then(response => {
+      console.log(response),
+      response.json()})
+    .then(data => {
+      console.log(data)
+      if (data.status === 'success') {
+        // Handle success
+        console.log('Success:', data);
+      } else {
+        // Handle errors
+        console.error('Error:', data.errors);
+      }
+    })
+    .catch(error => console.error('Error:', error));
+  }
+
+  delete(event) {
+    event.preventDefault()
+    this.likeTarget.classList.toggle("fa-solid")
+    this.likeTarget.classList.toggle("fa-regular")
+
+    const url = `/flats/${this.flatValue}/likes`
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    console.log(token)
+
+    const content = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        "flat_id": this.flatValue,
+        'X-CSRF-Token': token,
+      })
+    }
+
+    fetch(url, content)
+    .then(response => {
+      console.log(response),
+      response.json()})
+    .then(data => {
+      console.log(data)
+      if (data.status === 'success') {
+        // Handle success
+        console.log('Success:', data);
+      } else {
+        // Handle errors
+        console.error('Error:', data.errors);
+      }
+    })
+    .catch(error => console.error('Error:', error));
   }
 }
