401cf42deb229afb946ec7748a38f14605d87de3
luis sensat <luis.sensat@gmail.com>
2024-02-28 18:26:58 +0100

    fixed booking and availability to add reviews

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :flat
   has_many :booking_dates, dependent: :destroy
 
-  validates :start_time, comparison: { greater_than: Date.today }
+  validates :start_time, comparison: { greater_than: Date.today.to_s }
   validates :end_time, comparison: { greater_than: :start_time }
   validates_uniqueness_of :user, scope: [:flat, :booking_dates]
 end
